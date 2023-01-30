b4e1f995caa5fc1ead4d44e1aa5fe32990a6edb0
haako <haako@computas.com>
2023-01-30 20:08:03 +0100

    Make server respond with pong when a request is set

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
 // Uncomment this block to pass the first stage
+use std::io::prelude::*;
 use std::net::TcpListener;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-   
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
-     for stream in listener.incoming() {
-         match stream {
-             Ok(_stream) => {
-                 println!("accepted new connection");
-             }
-             Err(e) => {
-                 println!("error: {}", e);
-             }
-         }
-     }
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut _stream) => {
+                println!("accepted new connection");
+                _stream.write("+PONG\r\n".as_bytes());
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
 }
